da6fb76d03bbc39fa3e3ce566a8e5d932de9c899
Menik <menikdisney@gmail.com>
2018-05-16 13:09:22 +0600

    Implement spring MVC

--- src/main/java/com/optimusprime/demoEx/bootstrap/DevBootstrap.java
+++ src/main/java/com/optimusprime/demoEx/bootstrap/DevBootstrap.java
@@ -2,8 +2,10 @@ package com.optimusprime.demoEx.bootstrap;
 
 import com.optimusprime.demoEx.module.Author;
 import com.optimusprime.demoEx.module.Book;
+import com.optimusprime.demoEx.module.Publisher;
 import com.optimusprime.demoEx.repositories.AuthorRepository;
 import com.optimusprime.demoEx.repositories.BookRepository;
+import com.optimusprime.demoEx.repositories.PublisherRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -13,10 +15,13 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -25,8 +30,15 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
     }
 
     public void initData(){
+
+        Publisher publisher= new Publisher();
+        publisher.setName("foo");
+
+        publisherRepository.save(publisher);
+
+
         Author eric = new Author("Eric","Evans");
-        Book ddd= new Book("Domain Driven Design","1234","Harper Collins");
+        Book ddd= new Book("Domain Driven Design","1234",publisher);
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
@@ -35,7 +47,7 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
 
         Author rod = new Author("Red","Johnson");
-        Book noEJB= new Book("J2EE Development without EJB","23444","Worx");
+        Book noEJB= new Book("J2EE Development without EJB","23444",publisher);
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
 
---
+++ src/main/java/com/optimusprime/demoEx/repositories/PublisherRepository.java
@@ -0,0 +1,7 @@
+package com.optimusprime.demoEx.repositories;
+
+import com.optimusprime.demoEx.module.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublisherRepository extends CrudRepository<Publisher,Long> {
+}
