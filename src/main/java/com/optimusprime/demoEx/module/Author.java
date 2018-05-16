f858834acc54c662eaa73b17441a6064a3ddfbdf
Menik <menikdisney@gmail.com>
2018-05-16 10:30:22 +0600

    Connect to H2 db and Many to Many Relationship without Join Columns

--- src/main/java/com/optimusprime/demoEx/module/Author.java
+++ src/main/java/com/optimusprime/demoEx/module/Author.java
@@ -1,12 +1,19 @@
 package com.optimusprime.demoEx.module;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
 public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String firstName;
     private String lastName;
 
+    @ManyToMany
     private Set<Book> books = new HashSet<>();
 
     public Author() {
@@ -21,6 +28,14 @@ public class Author {
         this.firstName = firstName;
         this.lastName = lastName;
         this.books = books;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getFirstName() {
